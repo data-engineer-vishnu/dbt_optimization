be7cde0e2f9eddf71f79f52d8e9cd6161df37630
sravandbt286@gmail.com <sravandbt286@gmail.com>
2026-01-28 09:33:26 +0000

    gyg

---
+++ models/staging/dim_customers_sort.sql
@@ -0,0 +1,10 @@
+{{ config(
+    materialized='table'
+) }}
+-- dbt_model: dim_customers_sort
+-- expected_behavior: sort_spill
+
+SELECT
+    *
+FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF100.CUSTOMER
+ORDER BY c_comment, c_address, c_name
